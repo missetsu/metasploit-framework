0bd817ea0c8a3384a61edf7bed77bd4e58dabc35
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2018-10-31 21:50:21 -0500

    Bump version

--- db/dest_schema.rb
+++ db/dest_schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161227212223) do
+ActiveRecord::Schema.define(version: 20161227212224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
